e03433845a6cff1f853e6692fda37a5421ad7390
benasphy <binidani1903@gmail.com>
2025-11-06 20:01:33 +0300

    Add bubblesort implementation

---
+++ Sorting/bubblesort.py
@@ -0,0 +1,20 @@
+def BubbleSort(arr):
+    n = len(arr)
+    
+    for i in range(n):
+        swapped = False
+        
+        for j in range(0, n-i-1):
+            if arr[j] > arr[j+1]:
+                arr[j], arr[j+1] = arr[j+1], arr[j]
+                swapped = True
+        if (swapped == False):
+            break
+                
+    
+
+
+arr = [5, 6, 1, 3]
+BubbleSort(arr)
+for i in range(len(arr)):
+    print("%d" % arr[i], end = " ")
