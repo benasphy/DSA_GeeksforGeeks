5be1c0a3a3a88582c04b293fd8a03ae4dca836ba
benasphy <binidani1903@gmail.com>
2025-12-22 18:56:22 +0300

    Row major traversing

---
+++ row_major_traversing.py
@@ -0,0 +1,10 @@
+matrix = [
+    [1,2,3],
+    [4,5,6],
+    [7,8,9]
+]
+
+for row_idx in range(len(matrix)):
+    for col_idx in range(len(matrix[0])):
+        print(matrix[row_idx][col_idx])
+    
